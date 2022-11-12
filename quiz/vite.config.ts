051497943e114e7bc83dc127369a7bae799129f8
Mateusz Budzisz <electroluxv2@gmail.com>
2022-11-12 14:21:50 +0100

    Initial structure

--- quiz/vite.config.ts
+++ quiz/vite.config.ts
@@ -1,5 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-
+    root: resolve(__dirname, 'src/pages'),
+    build: {
+        outDir: resolve(__dirname, 'dist'),
+        emptyOutDir: true,
+        target: 'esnext',
+        rollupOptions: {
+            input: {
+                home: resolve(__dirname, 'src/pages/index.html'), // List of quizzes
+                edit: resolve(__dirname, 'src/pages/edit/index.html'), // Create / edit selected quiz
+                quiz: resolve(__dirname, 'src/pages/quiz/index.html'), // View quiz summary
+                setup: resolve(__dirname, 'src/pages/setup/index.html'), // One time user setup
+                question: resolve(__dirname, 'src/pages/question.scss/index.html'), // Question page
+                ranking: resolve(__dirname, 'src/pages/ranking/index.html') // View ranking for quiz
+            }
+        }
+    }
 });
