b731c9e9f66ba6a45eca693b384d30c9f1be6d56
Mateusz Budzisz <electroluxv2@gmail.com>
2022-11-15 21:32:57 +0100

    Create quiz button

--- quiz/src/pages/edit/edit.ts
+++ quiz/src/pages/edit/edit.ts
@@ -1,3 +1,16 @@
-console.log('Edit');
+import { Quiz } from '../../shared/quiz';
+import { QuizManager } from '../../shared/quiz-manager';
 
-export {};
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+    const id = Math.trunc(Math.random() * 100 + 1);
+
+    const quiz: Quiz = {
+        description: 'Sample Quiz Sample Quiz Sample Quiz Sample Quiz Sample Quiz Sample Quiz Sample Quiz Sample Quiz',
+        id,
+        name: `Quiz #${id}`
+    };
+
+    QuizManager.save(quiz);
+});
