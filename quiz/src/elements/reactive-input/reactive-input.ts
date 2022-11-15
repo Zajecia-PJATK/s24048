fdbf2834bf4a14a6825b4d09f3cd4876deb0574a
Mateusz Budzisz <electroluxv2@gmail.com>
2022-11-15 21:22:39 +0100

    Preserve name between pages

--- quiz/src/elements/reactive-input/reactive-input.ts
+++ quiz/src/elements/reactive-input/reactive-input.ts
@@ -15,15 +15,20 @@ export class ReactiveInput extends WebComponent {
         super();
 
         this
-            .setAttributeHandler(Attrs.label, v => (this.label = v) && this.render())
-            .setAttributeHandler(Attrs.type, v => (this.type = v) && this.render())
-            .setAttributeHandler(Attrs.storage, v => this.storagePath = v);
+            .setAttributeHandler(Attrs.label, v => { this.label = v; this.tryRender() })
+            .setAttributeHandler(Attrs.type, v => { this.type = v; this.tryRender() })
+            .setAttributeHandler(Attrs.storage, v => { this.storagePath = v; this.tryRender() });
+    }
+
+    private tryRender() {
+        !!this.label && !!this.storagePath && this.render();
     }
 
     private render(): void {
         this.input = ElementBuilder
             .make<HTMLInputElement>('input')
             .withAttr('type', this.type!)
+            .withAttr('value', localStorage.getItem(this.storagePath!) ?? '')
             .withEventHandler('input', this.onInput.bind(this))
             .build();
 
@@ -35,6 +40,8 @@ export class ReactiveInput extends WebComponent {
                 .withChild(this.input)
                 .build()
             );
+
+        this.onInput(null!);
     }
 
     private onInput(_: InputEvent): void {
@@ -44,7 +51,7 @@ export class ReactiveInput extends WebComponent {
         this.dispatchEvent(new CustomEvent(ReactiveInput.stateChangeEventType, {
             bubbles: true,
             detail: {
-                valid: !(value?.trim() === '') // TODO: Validate data e.g. required and type
+                valid: value.trim() !== '' // TODO: Validate data e.g. required and type
             }
         }));
     }
--- quiz/src/pages/setup/setup.ts
+++ quiz/src/pages/setup/setup.ts
@@ -3,15 +3,15 @@ import { NavigationManager } from '../../shared/navigation-manager';
 import { Attrs } from '../../elements/attrs';
 import { ReactiveInput } from '../../elements/reactive-input/reactive-input';
 
-customElements.define('quiz-preview', QuizPreview);
-customElements.define('reactive-input', ReactiveInput);
-
 const quizPreview = document.querySelector('quiz-preview')!;
-const { quizId } = NavigationManager.params;
 
+const { quizId } = NavigationManager.params;
 quizPreview.setAttribute(Attrs.quizId, quizId);
 
 addEventListener(ReactiveInput.stateChangeEventType, event => {
     // @ts-ignore
     quizPreview.setAttribute(Attrs.disabled, (!event.detail.valid).toString());
 });
+
+customElements.define('quiz-preview', QuizPreview);
+customElements.define('reactive-input', ReactiveInput);
