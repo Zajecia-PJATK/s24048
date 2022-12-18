280aa2aee31f633888cf4fcb90147198e98e7017
ElectroluxV2 <electroluxv2@gmail.com>
2022-12-18 19:18:45 +0100

    textarea improvements

--- mardown-editor/src/elements/reactive-textarea/reactive-textarea.ts
+++ mardown-editor/src/elements/reactive-textarea/reactive-textarea.ts
@@ -12,6 +12,18 @@ export class ReactiveTextarea extends HTMLElement {
 
         this.child = textarea;
         this.child.addEventListener('input', this.resize.bind(this));
+
+        if (!this.hasAttribute('save')) { return; }
+        const key = this.getAttribute('save');
+
+        // Restore content initially
+        this.child.value = sessionStorage.getItem(key);
+
+        // Mock user input
+        queueMicrotask(() => this.child.dispatchEvent(new InputEvent('input')));
+
+        // Save content on each type
+        this.child.addEventListener('input', () => sessionStorage.setItem(key, this.child.value));
     }
 
     private resize() {
--- mardown-editor/src/index.ts
+++ mardown-editor/src/index.ts
@@ -1,3 +1,9 @@
 import { ReactiveTextarea } from './elements/reactive-textarea/reactive-textarea';
 
 customElements.define('reactive-textarea', ReactiveTextarea);
+
+const input: HTMLTextAreaElement = document.querySelector('textarea#input');
+
+input.addEventListener('input', () => {
+    console.log(input.value)
+});
