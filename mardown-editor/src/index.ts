6e6ad85692c6f900b2e9e8f5a042904a5f16c873
ElectroluxV2 <electroluxv2@gmail.com>
2022-12-18 21:08:55 +0100

    reactive header

--- mardown-editor/src/index.ts
+++ mardown-editor/src/index.ts
@@ -5,16 +5,15 @@ customElements.define('reactive-textarea', ReactiveTextarea);
 
 const inputTextArea: HTMLTextAreaElement = document.querySelector('textarea#input');
 const outputTextArea: HTMLTextAreaElement = document.querySelector('textarea#output');
+const conversion: HTMLHeadingElement = document.querySelector('h1#conversion');
 
 inputTextArea.addEventListener('input', () => {
     const value = inputTextArea.value;
     const template = document.createElement('template');
-
     template.innerHTML = value;
     const isHtml = template.content.children.length > 0;
+    conversion.textContent = isHtml ? 'HTML 2 MD' : 'MD 2 HTML';
 
     outputTextArea.value = HtmlMapper.html2md(template.content);
-
     outputTextArea.dispatchEvent(new InputEvent('input'))
-
 });
